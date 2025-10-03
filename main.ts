09c73f60fb5b6dc1b6fb901dedf97da8835ea8d1
RoboRace <roborace@sirious.nl>
2025-10-03 20:15:09 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,44 @@
  */
 
 //% color="#AA278D" weight=100
-namespace RemoteControl {
+namespace RemoteControl
+
+    //% block
+    export function up() :number {
+			return _up;
+    }
+    //% block
+    export function down() :number {
+			return _down;
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function init_HX1838(initialiseer_IR_variabelen: string) {
+		nul = 181
+   		een = 22
+    	twee = 25
+    	drie = 13
+    	vier = 12
+    	vijf = 24
+    	zes = 133
+    	zeven = 8
+    	acht = 199
+    	negen = 165
+    	op = 157
+    	neer = 21
+    	links = 221
+    	rechts = 61
+    	ok = 253
+    	ster = 189
+    	hekje = 173
+    }
+
+
+//% block
+export function init_Keyestudio (initialiseer_IR_variabelen: string) {
 let	_nul = 25
 let	_een = 93
 let _twee = 157
@@ -24,48 +61,5 @@ let	_rechts = 165
 let	_ok = 199
 let	_ster = 22
 let	_hekje = 13
-    //% block
-    export function up() :number {
-			return _up;
-    }
-    //% block
-    export function down() :number {
-			return _down;
-    }
-
-    // note that Caml casing yields lower case
-    // block text with spaces
-
-    //% block
-    export function init_HX1838() {
-	nul = 181
-    een = 22
-    twee = 25
-    drie = 13
-    vier = 12
-    vijf = 24
-    zes = 133
-    zeven = 8
-    acht = 199
-    negen = 165
-    op = 157
-    neer = 21
-    links = 221
-    rechts = 61
-    ok = 253
-    ster = 189
-    hekje = 173
-    }
-}
-
-
-
-function init_HX1838 (initialiseer_IR_variabelen: string) {
-
-}
-function init_Keyestudio (initialiseer_IR_variabelen: string) {
-
-}
-basic.forever(function () {
+} : void
 	
-})
