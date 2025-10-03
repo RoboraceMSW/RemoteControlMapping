2629469c3d4854ce391e6aac28d4b81e1434231f
RoboRace <roborace@sirious.nl>
2025-10-03 12:56:22 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,58 @@
+let nul = 0
+let een = 0
+let twee = 0
+let drie = 0
+let vier = 0
+let vijf = 0
+let zes = 0
+let zeven = 0
+let acht = 0
+let negen = 0
+let op = 0
+let neer = 0
+let links = 0
+let rechts = 0
+let ok = 0
+let ster = 0
+let hekje = 0
+function init_HX1838 (initialiseer_IR_variabelen: string) {
+    nul = 25
+    een = 93
+    twee = 157
+    drie = 29
+    vier = 221
+    vijf = 253
+    zes = 61
+    zeven = 31
+    acht = 21
+    negen = 9
+    op = 24
+    neer = 181
+    links = 8
+    rechts = 165
+    ok = 199
+    ster = 22
+    hekje = 13
+}
+function init_Keyestudio (initialiseer_IR_variabelen: string) {
+    nul = 181
+    een = 22
+    twee = 25
+    drie = 13
+    vier = 12
+    vijf = 24
+    zes = 133
+    zeven = 8
+    acht = 199
+    negen = 165
+    op = 157
+    neer = 21
+    links = 221
+    rechts = 61
+    ok = 253
+    ster = 189
+    hekje = 173
+}
+basic.forever(function () {
+	
+})
