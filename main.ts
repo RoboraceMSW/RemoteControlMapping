6f74780a356a4ed3329e7b5b408775409ea282f6
RoboRace <roborace@sirious.nl>
2025-10-03 21:53:34 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,52 +7,70 @@
 
 //% color="#AA278D" weight=100 icon="\uf1ec"
 namespace RemoteControl {
+	let _nul = 0
+   	let _een = 0
+    let _twee = 0
+    let _drie = 0
+    let _vier = 0
+    let _vijf = 0
+    let _zes = 0
+    let _zeven = 0
+    let _acht = 0
+    let _negen = 0
+    let _op = 0
+    let _neer = 0
+    let _links = 0
+    let _rechts = 0
+    let _ok = 0
+    let _ster = 0
+    let _hekje = 0
 
 
     // note that Caml casing yields lower case
     // block text with spaces
 
     //% block
-	let _nul = 181
-   	let _een = 22
-    let _twee = 25
-    let _drie = 13
-    let _vier = 12
-    let _vijf = 24
-    let _zes = 133
-    let _zeven = 8
-    let _acht = 199
-    let _negen = 165
-    let _op = 157
-    let _neer = 21
-    let _links = 221
-    let _rechts = 61
-    let _ok = 253
-    let _ster = 189
-    let _hekje = 173
+
     export function init_HX1838() : void {
-    } 
+	 _nul = 181
+   	 _een = 22
+     _twee = 25
+     _drie = 13
+     _vier = 12
+     _vijf = 24
+     _zeven = 8
+     _acht = 199
+     _negen = 165
+     _op = 157
+     _neer = 21
+     _links = 221
+     _rechts = 61
+     _ok = 253
+     _ster = 189
+     _hekje = 173
+	} 
 
 
 //% block
-	let	_nul = 25
-	let	_een = 93
-	let _twee = 157
-	let _drie = 29
-	let	_vier = 221
-	let	_vijf = 253
-	let	_zes = 61
-	let	_zeven = 31
-	let	_acht = 21
-	let	_negen = 9
-	let	_op = 24
-	let	_neer = 181
-	let	_links = 8
-	let	_rechts = 165
-	let	_ok = 199
-	let	_ster = 22
-	let	_hekje = 13
+
 export function init_Keyestudio () : void {	
+	_nul = 25
+	_een = 93
+	 _twee = 157
+	 _drie = 29
+	_vier = 221
+	_vijf = 253
+	_zes = 61
+	_zeven = 31
+	_acht = 21
+	_negen = 9
+	_op = 24
+	_neer = 181
+	_links = 8
+	_rechts = 165
+	_ok = 199
+	_ster = 22
+	_hekje = 13
 	}	
 }
 
